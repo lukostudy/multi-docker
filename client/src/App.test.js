966d2a638b3e78042ae50652dffa31ca8eec4534
lukostudy <lukostudy@gmail.com>
2023-12-29 20:12:37 +0100

    minor updates in comments

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -3,7 +3,7 @@ import App from './App';
 
 // the test substituted due to potential crash in build environment
 // in reality this should be mocked
-test('renders learn react link', () => {}); // a;ways passes
+test('renders learn react link', () => {}); // always passes
 // test('renders learn react link', () => {
 //   render(<App />);
 //   const linkElement = screen.getByText(/learn react/i);
